a267f897d735afd86cf6ab25df8c8d06e28e3de7
Matt Mahowald <mahowaldmatt@gmail.com>
2023-03-07 13:16:33 -0600

    gunicorn

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<p>Box of Bits</p>"
 
 @app.route('/json', methods=['POST'])
 def post_data():
-    print("hello")
     if not request.is_json:
         return "bad request :(", 403
     content = request.get_json()
     return f"{content}", 201
+
+
+@app.route('/test/<word>/')
+def capitalize(word):
+    return f'<h1>{word}</h1>', 200
